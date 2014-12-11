7679301f7ed4e0fffcbe502fc98701dd6a66f311
Adrian Sampson <adrian@radbox.org>
2014-12-10 16:36:39 -0800

    Hello world parser

--- src/fake.rs
+++ src/fake.rs
@@ -1,3 +1,20 @@
+#![feature(phase)]
+#[phase(plugin)]
+extern crate peg_syntax_ext;
+
+pub struct Something {
+    pub name: String
+}
+
+peg! grammar(r#"
+use super::Something;
+
+#[pub]
+stuff -> Something
+    = [a-z]+ { Something { name: match_str.into_string() } }
+"#)
+
 fn main() {
-    println!("hi");
+    let res = grammar::stuff("hello");
+    println!("{}", res.unwrap().name);
 }
