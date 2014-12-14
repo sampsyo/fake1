3f8d5b305929e90e392a61ca55b9e6e437282d1d
Adrian Sampson <adrian@radbox.org>
2014-12-14 00:06:10 -0800

    Parse a recipe

--- src/fake.rs
+++ src/fake.rs
@@ -3,21 +3,31 @@
 extern crate peg_syntax_ext;
 
 pub struct Rule {
-    pub target: Expr
+    pub target: Expr,
+    pub recipe: Vec<Recipe>
 }
 
 pub struct Expr {
     pub value: String
 }
 
+pub struct Recipe {
+    pub line: String
+}
+
 peg! grammar(r#"
 use super::Rule;
 use super::Expr;
+use super::Recipe;
 
 #[pub]
 rule -> Rule
-    = t:expr ws ":" ws d:expr
-    { Rule { target: t } }
+    = t:expr ws ":" ws d:expr ws r:recipe*
+    { Rule { target: t, recipe: r } }
+
+recipe -> Recipe
+    = "\n" [ \t]+ l:line
+    { Recipe { line: l } }
 
 ws -> ()
     = [ \t]*
@@ -29,9 +39,14 @@ expr -> Expr
 ident -> String
     = [A-Za-z0-9_-]+
     { match_str.into_string() }
+
+line -> String
+    = [A-Za-z0-9 \t_-]+
+    { match_str.into_string() }
 "#)
 
 fn main() {
-    let res = grammar::rule("foo: bar");
-    println!("{}", res.unwrap().target.value);
+    let res = grammar::rule("foo: bar\n    baz\n    qux");
+    let rule = res.unwrap();
+    println!("{} {}", rule.target.value, rule.recipe[0].line);
 }
